702590bc730d085ef7eb0153ad2fc100181abb1e
parMaster <tony.racomore@gmail.com>
2021-01-15 17:42:30 +0200

    Problem solution and new big testcase

--- 11.Ransom_Note/11-ransom-note.go
+++ 11.Ransom_Note/11-ransom-note.go
@@ -4,9 +4,27 @@ import "fmt"
 
 func checkMagazine(magazine []string, note []string) {
 
-	fmt.Println("")
+	m := make(map[string]int, 0)
+
+	for _, mWord := range magazine {
+		m[mWord]++
+	}
+
+	for _, nWord := range note {
+
+		if 0 == m[nWord] {
+			fmt.Println("No")
+			return
+		}
+		m[nWord]--
+
+	}
+	fmt.Println("Yes")
+	return
+
 }
 
 func main() {
 
+	checkMagazine([]string{"give", "me", "one", "grand", "today", "night"}, []string{"give", "one", "grand", "today"})
 }
