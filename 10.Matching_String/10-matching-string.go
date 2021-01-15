48e880ae38f549dfa1a4a207a9a91d73173c4529
parMaster <tony.racomore@gmail.com>
2021-01-15 15:08:49 +0200

    passed the tests, solved the problem

--- 10.Matching_String/10-matching-string.go
+++ 10.Matching_String/10-matching-string.go
@@ -2,9 +2,23 @@ package main
 
 func matchingStrings(strings []string, queries []string) []int32 {
 
-	return []int32{0}
+	var m map[string]int32
+	m = make(map[string]int32)
+
+	for _, str := range strings {
+		m[str]++
+	}
+
+	result := []int32{}
+	for _, q := range queries {
+		result = append(result, m[q])
+	}
+
+	return result
 }
 
 func main() {
+
+	matchingStrings([]string{"aba", "baba", "aba", "xzxb"}, []string{"aba", "xzxb", "ab"})
 
 }
