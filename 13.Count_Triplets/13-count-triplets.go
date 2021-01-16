e0219284bd52d20a8576794f2c52426ec1da19f3
parMaster <tony.racomore@gmail.com>
2021-01-17 00:52:52 +0200

    test cases

---
+++ 13.Count_Triplets/13-count-triplets.go
@@ -0,0 +1,11 @@
+package main
+
+func countTriplets(arr []int64, r int64) int64 {
+
+	return 0
+
+}
+
+func main() {
+
+}
---
+++ 13.Count_Triplets/13-count-triplets_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+)
+
+type testpair struct {
+	arr      []int64
+	r        int64
+	expected int64
+}
+
+var tests = []testpair{
+	{[]int64{1, 2, 2, 4}, 2, 2},
+	{[]int64{1, 3, 9, 9, 27, 81}, 3, 6},
+	{[]int64{1, 5, 5, 25, 125}, 5, 4},
+}
+
+func TestCountTriplets(t *testing.T) {
+	for _, pair := range tests {
+		v := countTriplets(pair.arr, pair.r)
+		if v != pair.expected {
+			t.Error(
+				"For", pair.arr, pair.r,
+				"expected", pair.expected,
+				"got", v,
+			)
+		}
+	}
+}
