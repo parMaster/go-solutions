97f36f2c99c0f1bbad3443447e7e106531912c4f
parMaster <tony.racomore@gmail.com>
2021-01-18 01:02:59 +0200

    timeout on a huge test - I guess the bottleneck in the qsort

--- 15.Activity_Notifications/15-activity-notifications.go
+++ 15.Activity_Notifications/15-activity-notifications.go
@@ -4,20 +4,31 @@ import (
 	"math/rand"
 )
 
-func activityNotifications(e []int32, d int32) int32 {
-
-	if len(e) == int(d) {
-		return 0
-	}
-
-	return 0
-}
-
 func main() {
 	//
 }
 
+func activityNotifications(e []int32, d int32) int32 {
+
+	var result int32 = 0
+
+	// since 1 <= d <= n
+	if len(e) == int(d) {
+		return result
+	}
+
+	for i := d; i < int32(len(e)); i++ {
+
+		if float32(e[i]) >= 2*median(e[i-d:i]) {
+			result++
+		}
+	}
+
+	return result
+}
+
 func median(a []int32) float32 {
+	a = quicksort(a)
 	if 0 == len(a)%2 {
 		return float32((a[int(len(a)/2)])+a[int(len(a)/2)-1]) / 2
 	}
@@ -35,7 +46,7 @@ func quicksort(a []int32) []int32 {
 
 	a[pivot], a[right] = a[right], a[pivot]
 
-	for i, _ := range a {
+	for i := range a {
 		if a[i] < a[right] {
 			a[left], a[i] = a[i], a[left]
 			left++
