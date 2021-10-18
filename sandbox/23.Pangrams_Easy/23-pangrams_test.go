8553c48d3e02770034fd35058855019df070e6a3
parMaster <tony.racomore@gmail.com>
2021-10-18 15:52:11 +0300

    Pangrams problem from HR: https://www.hackerrank.com/challenges/pangrams/problem

---
+++ sandbox/23.Pangrams_Easy/23-pangrams_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func pangrams(s string) string {
+	s = strings.ToLower(s)
+
+	m := make(map[byte]int)
+	for i := 0; i <= len(s)-1; i++ {
+
+		if s[i] == 32 {
+			continue
+		}
+		_, ok := m[s[i]]
+		if ok {
+			m[s[i]]++
+		} else {
+			m[s[i]] = 1
+		}
+	}
+
+	if len(m) == 26 {
+		return "pangram"
+	} else {
+		return "not pangram"
+	}
+
+}
+
+func Test_pangrams(t *testing.T) {
+	assert.Equal(t, "pangram", pangrams("We promptly judged antique ivory buckles for the next prize"))
+}
