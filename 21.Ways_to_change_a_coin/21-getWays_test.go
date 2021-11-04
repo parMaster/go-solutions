ca7a3e4f76c5f4ba12b3a746a668de22fd4675c8
parMaster <tony.racomore@gmail.com>
2021-11-04 23:18:23 +0200

    Done. I'm not proud of it, but it works and I understand how

--- 21.Ways_to_change_a_coin/21-getWays_test.go
+++ 21.Ways_to_change_a_coin/21-getWays_test.go
@@ -7,43 +7,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type memo struct {
-	solutions map[int32]int64
-}
-
 // getWays from HackerRank
 func getWays(n int32, c []int64) int64 {
 
-	m := memo{}
-	m.solutions = make(map[int32]int64, n+1)
-	for i := int32(0); i <= n; i++ {
-		m.solutions[i] = m.getSubWays(n, c)
+	dp := make([]int64, n+1)
+	dp[0] = 1
+	for i, _ := range c {
+		for j := c[i]; j <= int64(n); j++ {
+			dp[j] += dp[j-c[i]]
+		}
 	}
-
-	return m.solutions[n]
-}
-
-func (m *memo) getSubWays(n int32, c []int64) int64 {
-
-	elem, ok := m.solutions[n]
-	if ok {
-		return elem
-	}
-
-	if n == 0 {
-		return 1
-	}
-
-	if n < 0 {
-		return 0
-	}
-
-	var ways int64 = 0
-	for i, v := range c {
-		ways += m.getSubWays(n-int32(v), c[i:])
-	}
-
-	return ways
+	return dp[n]
 }
 
 func Test_getWays(t *testing.T) {
@@ -56,7 +30,7 @@ func Test_getWays(t *testing.T) {
 		{int64(3), 3, []int64{8, 3, 1, 2}},
 		{int64(4), 4, []int64{1, 2, 3}},
 		{int64(5), 10, []int64{2, 5, 3, 6}},
-		// {int64(96190959), 166, []int64{5, 37, 8, 39, 33, 17, 22, 32, 13, 7, 10, 35, 40, 2, 43, 49, 46, 19, 41, 1, 12, 11, 28}},
+		{int64(96190959), 166, []int64{5, 37, 8, 39, 33, 17, 22, 32, 13, 7, 10, 35, 40, 2, 43, 49, 46, 19, 41, 1, 12, 11, 28}},
 	}
 
 	for _, p := range testPairs {
@@ -64,11 +38,67 @@ func Test_getWays(t *testing.T) {
 		assert.Equal(t, p.expected, getWays(p.targetSum, p.numbers))
 	}
 
-	// timeout on HR, 16 seconds on Xeon
-	// {int64(96190959), 166, []int64{5, 37, 8, 39, 33, 17, 22, 32, 13, 7, 10, 35, 40, 2, 43, 49, 46, 19, 41, 1, 12, 11, 28}},
-
 }
 
-//i func getWays(n int, c []int) int {
+/*
 
-// }
+ 0  1  2  3  4  5  6  7  8  9  10
+init
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+i == 1, coins[i] == 2
+[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0] j == 2..n; dp[2] += dp[0] // so 1
+[1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0] dp[3] += dp[1] // so 0
+[1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0] // 1
+[1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0]
+[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+if there is just one coin = 2 (coins is {2}) , there is 1 way to change 2,4,6,8,10 Euro coins - 1*2, 2*2, 3*2, 4*2, or 5*2
+
+i==2, coins[2] == 5
+[1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1] start with 5, dp[5] += dp[5-5]
+[1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1] dp[6] += dp[6-5] // 1+=0
+[1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1] dp[7] += dp[7-5] // 0+=1 = 1
+[1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1] dp[8]
+[1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1] dp[9]
+[1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 2] dp[10] += dp[10-5] // 1+=1 = 2
+i==3, coins[3] == 3
+[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2] dp[3] += dp[3-3] // 0+1
+[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2]
+[1, 0, 1, 1, 1, 2, 1, 1, 1, 1, 2]
+[1, 0, 1, 1, 1, 2, 2, 1, 1, 1, 2]
+[1, 0, 1, 1, 1, 2, 2, 2, 1, 1, 2]
+[1, 0, 1, 1, 1, 2, 2, 2, 3, 1, 2]
+[1, 0, 1, 1, 1, 2, 2, 2, 3, 3, 2]
+[1, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4]
+i==4, coins[4] == 6
+[1, 0, 1, 1, 1, 2, 3, 2, 3, 3, 4] dp[6] = dp[6] + dp[6-6] = 2+1
+[1, 0, 1, 1, 1, 2, 3, 2, 3, 3, 4]
+[1, 0, 1, 1, 1, 2, 3, 2, 4, 3, 4]
+[1, 0, 1, 1, 1, 2, 3, 2, 4, 4, 4]
+[1, 0, 1, 1, 1, 2, 3, 2, 4, 4, 5] dp[10] = dp[10] + dp[10-6] = dp[10] + dp[4] = 4+1
+
+*/
+
+// works perfectly at fast machines with long stacks
+// but results in half of the tests being failed due to time limit
+func getWays_recursive(n int32, c []int64) int64 {
+
+	if n == 0 {
+		return 1
+	}
+
+	if n < 0 {
+		return 0
+	}
+
+	var ways int64 = 0
+	for i, v := range c {
+		ways += getWays(n-int32(v), c[i:])
+	}
+
+	return ways
+}
