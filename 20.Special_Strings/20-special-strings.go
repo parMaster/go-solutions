2d45b999ad2909f37db2add827773c50c8272144
parMaster <tony.racomore@gmail.com>
2021-11-22 17:59:06 +0200

    Solved. Finally! I was so close back then))

--- 20.Special_Strings/20-special-strings.go
+++ 20.Special_Strings/20-special-strings.go
@@ -9,71 +9,55 @@ type Super struct {
 	n        int32
 	str      string
 	subCount int64
-	specials map[string]int
 }
 
-// SearchAround is method
-func (s *Super) SearchAround(pos int) int64 {
-
-	i, found, letterAround := 0, 1, ""
-	s.subCount++
-	for 1 == found {
+// SearchAround finds a number of anagram around `pos` character
+func (s *Super) SearchAround(pos int) {
+	i, found, specialLetter := 0, int64(1), ""
+	for found == 1 {
 		found = 0
 		i++
 		if (0 <= pos-i) &&
 			pos+i < len(s.str) &&
 			s.str[pos-i] == s.str[pos+i] &&
-			(letterAround == "" || letterAround == string(s.str[pos-i])) {
+			(specialLetter == "" || specialLetter == string(s.str[pos-i])) {
 
-			letterAround = string(s.str[pos-i])
-			// builtString = string(s.str[pos-i]) + builtString + string(s.str[pos+i])
-			// fmt.Println(builtString)
-			// _, ok := s.specials[builtString]
-			// if !ok {
+			specialLetter = string(s.str[pos-i])
 			s.subCount++
-			// s.specials[builtString] = 1
-			// }
 			found = 1
-
 		}
 	}
-	return 0
 }
 
-// SearchForward is method
-func (s *Super) SearchForward(pos int) int64 {
+// SearchForward is trying to construct the longest string of the same chars starting from pos
+// Gauss(?) formula to determine the number of possible substring in a string of lenght N:
+// SubstrCount = (n*(n+1))/2
+// returns new position to continue from (don't use SearchAround for these kind of strings)
+func (s *Super) SearchForward(pos int) int {
 
 	i, found := 0, 1
-	for 1 == found {
+	for found == 1 {
 		found = 0
 		i++
 		if pos+i < len(s.str) &&
 			s.str[pos] == s.str[pos+i] {
-			s.subCount++
 			found = 1
 		}
 	}
-	return 0
+	s.subCount += int64((i * (i + 1)) / 2)
+	return pos + i - 1
 }
 
 func substrCount(n int32, s string) int64 {
 
-	superString := Super{n, s, 0, map[string]int{}}
+	superString := Super{n, s, 0}
 	for i := 0; i < len(s); i++ {
+		i = superString.SearchForward(i)
 		superString.SearchAround(i)
-		superString.SearchForward(i)
 	}
-
-	// fmt.Println(superString.specials)
-
 	return superString.subCount
 }
 
 func main() {
 	fmt.Println("")
 }
-
-// Sample 2 (aaaa) and explanation are invalid
-// According to the Problem conditions, the answer should be 12 but not 10:
-// 1) all the characters are the same:
-// a, a, a, a, aa, aa, aa, aaa, aaa
