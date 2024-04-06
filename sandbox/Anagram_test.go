9174c92d9099eaead380d780525c69c8651cdf08
Dmytro Borshchanenko <tony.racomore@gmail.com>
2024-04-07 01:45:18 +0300

    diffCount insane person implementation

--- sandbox/Anagram_test.go
+++ sandbox/Anagram_test.go
@@ -24,10 +24,37 @@ func isAnagram(s1, s2 string) bool {
 }
 
 func Test_Anagram(t *testing.T) {
-
 	assert.True(t, isAnagram("schoolmaster", "the classroom"), "the classroom")
 	assert.True(t, isAnagram("punishment", "nine thumps"), "nine thumps")
 	assert.True(t, isAnagram("debit card", "bad credit"), "bad credit")
 	assert.False(t, isAnagram("debit card", "good credit"), "good credit")
+}
 
+// how many characters to delete from both strings to get anagram
+// insane person implementation with only one minimal hashmap
+func diffCount(s1, s2 string) int {
+	if len(s2) > len(s1) {
+		s1, s2 = s2, s1
+	}
+
+	m := map[string]int{}
+	for _, r := range s1 {
+		s := string(r)
+		if _, ok := m[s]; !ok {
+			m[s] = strings.Count(s1, s) - strings.Count(s2, s)
+		}
+	}
+	sum := 0
+	for _, count := range m {
+		sum += count
+	}
+	diff := len(s2) - (len(s1) - sum) + sum
+
+	return diff
+}
+
+func Test_MinAnagram(t *testing.T) {
+	assert.Equal(t, 6, diffCount("hello", "billion"))
+	assert.Equal(t, 7, diffCount("hello", "billlion"))
+	assert.Equal(t, 0, diffCount("llo", "lol"))
 }
