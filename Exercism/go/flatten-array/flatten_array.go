a74b5c4611844ae958666f5f1fa2776cfabc7355
parmaster <tony.racomore@gmail.com>
2024-06-21 13:43:02 +0300

    Flatten v2

--- Exercism/go/flatten-array/flatten_array.go
+++ Exercism/go/flatten-array/flatten_array.go
@@ -1,6 +1,23 @@
 package flatten
 
+// switch case on type
 func Flatten(nested interface{}) []interface{} {
+	output := []interface{}{}
+
+	for _, el := range nested.([]interface{}) {
+		switch el.(type) {
+		case int:
+			output = append(output, el)
+		case []interface{}:
+			output = append(output, Flatten(el)...)
+		}
+	}
+
+	return output
+}
+
+// checking if type casting is successful
+func Flatten_v1(nested interface{}) []interface{} {
 	output := []interface{}{}
 	input, ok := nested.([]interface{})
 	if !ok {
